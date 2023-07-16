cc89250ed5c048e91eefe63a61d35cd1f7762ea5
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-16 17:08:23 -0300

    add status bar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import { StatusBar } from 'react-native'
 
 import { Home } from '@screens/Home';
 import { Loading } from '@components/Loading';
@@ -9,6 +10,11 @@ export default function App() {
   const [fontLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold });
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"dark-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
       {fontLoaded ? <Home /> : <Loading />} 
     </ThemeProvider>
   );
