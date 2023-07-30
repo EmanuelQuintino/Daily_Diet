1ed3cf7ff96ad85b0fc3296e4e654c668c4ee882
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-30 12:54:21 -0300

    add safe area

--- src/screens/Statistics/styles.ts
+++ src/screens/Statistics/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background: ${({ theme }) => theme.COLORS.WHITE};
   align-items: center;
