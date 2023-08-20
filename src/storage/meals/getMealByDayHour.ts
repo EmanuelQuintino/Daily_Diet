18e17b1ac1785417c2c1e970662f65aef83a46a7
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-08-20 13:50:01 -0300

    refactor getMealByDayHours

--- src/storage/meals/getMealByDayHour.ts
+++ src/storage/meals/getMealByDayHour.ts
@@ -9,17 +9,19 @@ export async function getMealByDayHours(day: string, hour: string) {
     const meals: MealsDataProps = storage ? JSON.parse(storage) : [];
 
     const mealFindByDay = meals.find((item) => item.day === day);
-    const mealFindByHour = mealFindByDay
-      ? mealFindByDay.data.find((meal) => meal.hour === hour)
-      : [];
-    
-    if (mealFindByHour) {
-      return {
-        day,
-        data: mealFindByHour
+
+    if (mealFindByDay) {
+      const mealFindByHour = mealFindByDay.data.find(
+        (meal) => meal.hour === hour
+      );
+
+      if (mealFindByHour) {
+        return {
+          day,
+          data: mealFindByHour,
+        };
       }
     }
-    
     return undefined;
   } catch (error) {
     throw error;
